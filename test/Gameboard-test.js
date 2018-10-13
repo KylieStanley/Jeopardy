ffb59c030ba2d9a0de2fed7f1452ea3b84e7c225
Kylie Stanley <knstanley11@gmail.com>
2018-10-12 19:01:44 -0600

    Refactor generate categories function to be a method on the Game class and add some tests for Game

---
+++ test/Gameboard-test.js
@@ -0,0 +1,17 @@
+const chai = require('chai')
+const expect = chai.expect;
+const Gameboard = require('../lib/Gameboard.js')
+
+describe('Gameboard', function() {
+  it('should instantiate a new Gameboard', function() {
+    let gameboard = new Gameboard();
+    expect(gameboard).to.exist;
+  });
+});
+
+describe('Gameboard', function() {
+  it('should generate random categories', function() {
+    let gameboard = new Gameboard();
+
+  })
+})
