0f82b8afdecae4a9ecea41cf7e153560ffa43086
Kylie Stanley <knstanley11@gmail.com>
2018-10-14 16:49:50 -0600

    Separate out daily double and question class

--- test/DailyDouble-test.js
+++ test/DailyDouble-test.js
@@ -1,23 +1,23 @@
-// const chai = require('chai')
-// const expect = chai.expect;
-// const DailyDouble = require('../lib/DailyDouble.js')
-// global.Question = require('../lib/Question.js')
-// const data = require('../lib/jeopardy-datasets.js')
+const chai = require('chai')
+const expect = chai.expect;
+const DailyDouble = require('../lib/DailyDouble.js')
+global.Question = require('../lib/Question.js')
+const data = require('../lib/jeopardy-datasets.js')
 
-// describe('hooks', function() {
-//   let question
-//   let dailyDouble;
-//   beforeEach(function() {
-//     question = new Question();
-//     dailyDouble = new DailyDouble();
-//   })
+describe('hooks', function() {
+  let question
+  let dailyDouble;
+  beforeEach(function() {
+    question = new Question();
+    dailyDouble = new DailyDouble();
+  })
 
 
-//   describe('DailyDouble', function() {
-//     it('should instantiate a new DailyDouble', function() {
-//       expect(dailyDouble).to.exist;
-//     });
-//   });
-// });
+  describe('DailyDouble', function() {
+    it('should instantiate a new DailyDouble', function() {
+      expect(dailyDouble).to.exist;
+    });
+  });
+});
 
 
--- test/Question-test.js
+++ test/Question-test.js
@@ -1,5 +1,3 @@
-//Daily double not defined, can't get tests to pass
-
 const chai = require('chai')
 const expect = chai.expect;
 global.Question = require('../lib/Question.js')
@@ -7,23 +5,13 @@ const data = require('../lib/jeopardy-datasets.js')
 
 describe('hooks', function() {
   let question;
-  // let dailyDouble;
   beforeEach(function() {
     question = new Question();
-    // dailyDouble = new DailyDouble();
   })
 
   describe('Question', function() {
     it('should instantiate a new question', function() {
       expect(question).to.exist;
-    });
-  });
-
-
-describe('DailyDouble', function() {
-    it('should instantiate a new DailyDouble', function() {
-      let dailyDouble = new DailyDouble();
-      expect(dailyDouble).to.exist;
     });
   });
 
