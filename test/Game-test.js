bbb278b54d9cc179d57f3d1703b354e3774ec51e
Kylie Stanley <knstanley11@gmail.com>
2018-10-17 14:05:13 -0600

    Add tests for Game class

--- test/Game-test.js
+++ test/Game-test.js
@@ -15,8 +15,11 @@ chai.spy.on(global.domUpdates,
 
 describe('hooks', function() {
   let game;
+  let player;
   beforeEach(function() {
     game = new Game();
+    player = new Player('1')
+    game.generatePlayers(player, player, player)
   })
 
   describe('Game', function() {
@@ -25,21 +28,30 @@ describe('hooks', function() {
     });
  
     it('should have three players', function() {
-      let player = new Player('1')
-      game.generatePlayers(player, player, player)
       expect(game.players).to.eql([player, player, player]);
-    });
-
-    it('should have 4 categories', function() {
-      game.generateRandomCategories(data);
-      expect(game.categories.length).to.equal(4);
     });
 
     it('should set up players and board', function() {
       game.setUpGame()
       expect(domUpdates.renderScoreboard).to.have.been.called(1)
       expect(domUpdates.renderGameboard).to.have.been.called(1)
-    })
-  })
+    });
 
+    it('should have 4 categories', function() {
+      game.generateRandomCategories();
+      expect(game.categories.length).to.equal(4);
+    });
+
+    it('should generate 20 questions', function() {
+      game.generateRandomCategories()
+      game.generateQuestions();
+      expect(game.questions.length).to.equal(20);
+    });
+
+    it('should know which player\'s turn it is', function() {
+      expect(game.playerTurn).to.equal(0)
+      game.changeTurn()
+      expect(game.playerTurn).to.equal(1)
+    });
+  });
 });
